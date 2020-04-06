0b0a284d0f0d1ffa2fc937c545e46ffcff4d6428
agungdhewe <agung_dhewe@yahoo.com>
2020-04-06 07:18:43 +0700

    check root dir

--- public/index.php
+++ public/index.php
@@ -8,12 +8,13 @@ error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 date_default_timezone_set('Asia/Jakarta');
 
 
-
 define('FGTA4', 1);
 define('__APPNAME', 'kalista');
 define('__ROOT_DIR', realpath(dirname(__FILE__).'/..'));
 define('__BASEADDRESS', $_SERVER['REQUEST_SCHEME'] ."://".  $_SERVER['SERVER_NAME'] . rtrim($_SERVER['SCRIPT_NAME'], '/index.php') .'/');
 define('API_LOGIN_URL', 'fgta/framework/login/dologin');
+
+
 
 
 define('DB_CONFIG_PARAM', [
