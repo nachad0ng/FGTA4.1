06b17dd774e88c21ef454f26f7f2a774251201cd
agungdhewe <agung_dhewe@yahoo.com>
2020-05-07 14:32:03 +0700

    update dbconfig

--- public/dbconfig.php
+++ public/dbconfig.php
@@ -1,29 +1,5 @@
 <?php namespace FGTA4;
 
-// define('DB_CONFIG', [
-// 	'FGTA' => [
-// 	],
-
-// 	'DSR' => [
-// 		'DSN' => "firebird:dbname=172.18.10.11:DSR.FDB",
-// 		'user' => "SYSDBA",
-// 		'pass' => "Modul@Oblongata"
-// 	],
-
-// 	'KALISTA' => [
-// 		'DSN' => "mysql:host=kalistadb-dev;dbname=kalista",
-// 		'user' => "root",
-// 		'pass' => "rahasia"
-// 	],
-
-// 	'ETAP' => [
-// 		'DSN' => "mysql:host=kalistadb-dev;dbname=etapdb",
-// 		'user' => "root",
-// 		'pass' => "rahasia"
-// 	]	
-// ]);
-
-
 define('DB_CONFIG', [
 	'FGTACLOUD' => [
 		'DSN' => "mysql:host=localhost;dbname=fgtadb",
